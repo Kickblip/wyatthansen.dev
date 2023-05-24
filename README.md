aed05343849e80492cbff91f23a5ec35ba04033a
Kickblip <wyatt.zilker@gmail.com>
2023-05-24 15:35:33 -0500

    imagesssss

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Next.js + Contentlayer
 
-A template with Next.js 13 app dir, Contentlayer, Tailwind CSS and dark mode.
+Cloned from Vercel's Next.js 13, Contentlayer template. Modified by Kickblip.
+
+Template available here:
 
 https://next-contentlayer.vercel.app
-
-
--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,21 @@ import { allPosts } from "@/.contentlayer/generated"
 import Link from "next/link"
 
 export default function Home() {
-  return (
-    <div className="prose dark:prose-invert">
-      {allPosts.map((post) => (
-        <article key={post._id}>
-          <Link href={post.slug}>
-            <h2>{post.title}</h2>
-          </Link>
-          {post.description && <p>{post.description}</p>}
-        </article>
-      ))}
-    </div>
-  )
+    return (
+        <div className="prose dark:prose-invert">
+            {allPosts.map((post) => (
+                <article key={post._id} className="flex items-start space-x-4">
+                    <div className="aspect-w-16 aspect-h-9 w-[40%] overflow-hidden">
+                        <img className="object-cover rounded w-full h-full" src={post.cover} alt={post.title} />
+                    </div>
+                    <div className="w-[60%]">
+                        <Link href={post.slug} className="no-underline">
+                            <h2 className="text-2xl font-bold hover:underline cursor-pointer">{post.title}</h2>
+                        </Link>
+                        {post.description && <p>{post.description}</p>}
+                    </div>
+                </article>
+            ))}
+        </div>
+    )
 }
