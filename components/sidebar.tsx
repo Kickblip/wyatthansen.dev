b8aa78a74350bb57a970017a2f264961232c1294
Kickblip <wyatt.zilker@gmail.com>
2023-05-29 14:42:35 -0500

    conditional buttons

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link"
+import Image from "next/image"
 
 interface LinkProp {
     href: string
@@ -12,24 +13,26 @@ interface SidebarMenuProps {
 
 export function SidebarMenu({ github, live }: SidebarMenuProps) {
     return (
-        <div className="w-64 p-4">
+        <div className="w-64 p-4 flex-col">
             <nav>
-                <ul>
-                    {github && (
-                        <li>
-                            <Link href={github.href}>
-                                <button className="p-2 bg-blue-500 text-white rounded">{github.text}</button>
-                            </Link>
-                        </li>
-                    )}
-                    {live && (
-                        <li>
-                            <Link href={live.href}>
-                                <button className="p-2 bg-blue-500 text-white rounded">{live.text}</button>
-                            </Link>
-                        </li>
-                    )}
-                </ul>
+                {github && (
+                    <Link
+                        href={github.href}
+                        className="inline-flex items-center px-4 py-2 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out block mb-2"
+                    >
+                        <Image src="/icons/github-dark.png" alt="Github Icon" width={20} height={20} className="mr-2" />
+                        {github.text}
+                    </Link>
+                )}
+                {live && (
+                    <Link
+                        href={live.href}
+                        className="inline-flex items-center px-4 py-2 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out block mb-2"
+                    >
+                        <Image src="/icons/external-dark.png" alt="Live Icon" width={20} height={20} className="mr-2" />
+                        {live.text}
+                    </Link>
+                )}
             </nav>
         </div>
     )
