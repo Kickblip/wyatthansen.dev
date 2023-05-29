cfdb2ea4b4a6643fc12f56fa403956bbf886d592
Kickblip <wyatt.zilker@gmail.com>
2023-05-29 13:10:12 -0500

    additional posts at footer of post

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,7 @@
 import { allPosts } from "@/.contentlayer/generated"
 import Link from "next/link"
 import HomeIntro from "@/components/home-intro"
-
-const tagColors: {
-    [key: string]: string
-} = {
-    TypeScript: "#7dd3fc",
-    NextJS: "#fcd34d",
-    TailwindCSS: "#7dd3fc",
-    Pinecone: "#fca5a5",
-    LangChain: "#c4b5fd",
-    MongoDB: "#6ee7b7",
-    JavaScript: "#f7df1e",
-    Node: "#6ee7b7",
-    APIs: "#fca5a5",
-    Phaser3: "#feba74",
-    ColyseusIO: "#c4b5fd",
-    Python: "#7dd3fc",
-    OpenCV: "#faa8d4",
-    TFLite: "#feba74",
-    "Raspberry Pi": "#d4d4d4",
-    Puppeteer: "#f7df1e",
-    AWS: "#7dd3fc",
-    FFmpeg: "#6ee7b7",
-    ThreeJS: "#feba74",
-}
+import { tagColors } from "./tags"
 
 export default function Home() {
     return (
--- app/posts/[...slug]/page.tsx
+++ app/posts/[...slug]/page.tsx
@@ -1,7 +1,9 @@
 import { notFound } from "next/navigation"
 import { allPosts } from "contentlayer/generated"
+import { tagColors } from "../../tags"
 
 import { Metadata } from "next"
+import Link from "next/link"
 import { Mdx } from "@/components/mdx-components"
 
 interface PostProps {
@@ -48,11 +50,53 @@ export default async function PostPage({ params }: PostProps) {
     }
 
     return (
-        <article className="py-6 prose dark:prose-invert">
+        <article className="py-6 prose dark:prose-invert mt-10">
+            <div className="mb-5">
+                {Object.entries(post.tags).map(([tag, active]) => (
+                    <span
+                        key={tag}
+                        className="inline-block rounded py-0.5 px-1 m-1 text-xs font-medium text-black"
+                        style={{ backgroundColor: active ? tagColors[tag] : "#eaeaea" }}
+                    >
+                        {tag}
+                    </span>
+                ))}
+            </div>
+
             <h1 className="mb-2">{post.title}</h1>
             {post.description && <p className="text-xl mt-0 text-slate-700 dark:text-slate-200">{post.description}</p>}
             <hr className="my-4" />
             <Mdx code={post.body.code} />
+
+            {allPosts
+                .filter((additionalPost) => additionalPost._id !== post._id)
+                .slice(0, 3)
+                .map((post) => (
+                    <article key={post._id} className="flex items-start space-x-8 w-full">
+                        <div className="aspect-w-16 aspect-h-9 w-[40%] overflow-hidden">
+                            <img className="object-cover rounded w-full h-full" src={post.cover} alt={post.title} />
+                        </div>
+                        <div className="w-[60%]">
+                            <div className="p-0 mt-10">
+                                {Object.entries(post.tags).map(([tag, active]) => (
+                                    <span
+                                        key={tag}
+                                        className="inline-block rounded py-0.5 px-1 m-1 text-xs font-medium text-black"
+                                        style={{ backgroundColor: active ? tagColors[tag] : "#eaeaea" }}
+                                    >
+                                        {tag}
+                                    </span>
+                                ))}
+                            </div>
+                            <Link href={post.slug} className="no-underline">
+                                <h2 className="text-2xl font-bold hover:underline cursor-pointe mb-1 mt-2">{post.title}</h2>
+                            </Link>
+                            <div className="text-sm dark:text-[#9c9c9c] text-[#737478]">
+                                {post.description && <p>{post.description}</p>}
+                            </div>
+                        </div>
+                    </article>
+                ))}
         </article>
     )
 }
