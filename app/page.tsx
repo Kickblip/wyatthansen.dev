b5c23c4b7cc3fdbfc8facd5c2e93697a66696636
Kickblip <wyatt.zilker@gmail.com>
2023-05-29 12:21:22 -0500

    more articles

--- app/page.tsx
+++ app/page.tsx
@@ -14,6 +14,16 @@ const tagColors: {
     JavaScript: "#f7df1e",
     Node: "#6ee7b7",
     APIs: "#fca5a5",
+    Phaser3: "#feba74",
+    ColyseusIO: "#c4b5fd",
+    Python: "#7dd3fc",
+    OpenCV: "#faa8d4",
+    TFLite: "#feba74",
+    "Raspberry Pi": "#d4d4d4",
+    Puppeteer: "#f7df1e",
+    AWS: "#7dd3fc",
+    FFmpeg: "#6ee7b7",
+    ThreeJS: "#feba74",
 }
 
 export default function Home() {
