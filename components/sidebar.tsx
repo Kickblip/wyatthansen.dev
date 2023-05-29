c2c648414d6694ef6c3a15f36e194f3b2d922c47
Kickblip <wyatt.zilker@gmail.com>
2023-05-29 16:50:50 -0500

    buttons styling

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -14,27 +14,29 @@ interface SidebarMenuProps {
 export function SidebarMenu({ github, live }: SidebarMenuProps) {
     return (
         <div className="w-64 p-4 flex-col">
-            <nav>
+            <nav className="space-y-4">
                 <Link
                     href={github?.href ?? "#"}
-                    className={`inline-flex items-center px-4 py-2 border border-transparent text-sm leading-5 font-medium rounded-md text-white ${
-                        github?.href ? "bg-indigo-600" : "bg-red-600"
-                    } hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out block mb-2`}
+                    target="_blank"
+                    className={`inline-flex items-center px-4 py-2 border border-black dark:border-white text-sm leading-5 font-medium rounded-md text-black dark:text-white ${
+                        github?.href ? "bg-transparent" : "bg-transparent"
+                    } hover:bg-indigo-500`}
                 >
                     <Image
                         src={github?.href ? "/icons/github.png" : "/icons/none.png"}
                         alt="Github Icon"
                         width={20}
                         height={20}
-                        className="mr-2 invert dark:invert-0"
+                        className="mr-2 invert-0 dark:invert"
                     />
-                    {github?.text ?? "No Link Provided"}
+                    {github?.text ?? "Closed Source"}
                 </Link>
                 <Link
                     href={live?.href ?? "#"}
-                    className={`inline-flex items-center px-4 py-2 border border-transparent text-sm leading-5 font-medium rounded-md text-white ${
-                        live?.href ? "bg-indigo-600" : "bg-red-600"
-                    } hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out block mb-2`}
+                    target="_blank"
+                    className={`inline-flex items-center px-4 py-2 border border-transparent text-sm leading-5 font-medium rounded-md text-white dark:text-black ${
+                        live?.href ? "bg-black dark:bg-white" : "bg-gray-700 dark:bg-gray-300"
+                    } hover:bg-indigo-500`}
                 >
                     <Image
                         src={live?.href ? "/icons/external.png" : "/icons/none.png"}
@@ -43,7 +45,7 @@ export function SidebarMenu({ github, live }: SidebarMenuProps) {
                         height={20}
                         className="mr-2 invert dark:invert-0"
                     />
-                    {live?.text ?? "No Link Provided"}
+                    {live?.text ?? "Project Not Live"}
                 </Link>
             </nav>
         </div>
